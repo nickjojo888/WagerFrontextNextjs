d0b014a4a45ec608e579348975f6e5f6f3b04396
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-08-25 15:40:54 +0100

    setup svgrwebpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/, // Test for .svg files
+      use: [
+        {
+          loader: "@svgr/webpack", // Use the svgr/webpack loader
+          options: {
+            icon: true, // Optional: Set this to true if your SVGs are primarily icons
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
