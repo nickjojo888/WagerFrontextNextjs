b32e8faaa44b6ef3214f0a03abff7a0a03076f6c
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-09-03 18:15:41 +0100

    optimise links in the user menu in header to user links from nextjs instead of router.push which causes a whole page reload

--- app/components/header/UserMenu.tsx
+++ app/components/header/UserMenu.tsx
@@ -1,39 +1,37 @@
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import ReceiptIcon from "@/public/svgs/receipt-outline.svg";
 import StarIcon from "@/public/svgs/star-outline.svg";
 import ChatboxIcon from "@/public/svgs/chatbox-outline.svg";
 import PersonIcon from "@/public/svgs/person-outline.svg";
 
 const UserMenu: React.FC = () => {
-  const router = useRouter();
-
   return (
     <div className="flex space-x-2">
-      <button
+      <Link
+        href="/transactions"
         className="bg-gray-800 hover:bg-gray-700 text-white p-2 rounded-full transition duration-300"
-        onClick={() => router.push("/transactions")}
       >
         <ReceiptIcon width={24} height={24} />
-      </button>
-      <button
+      </Link>
+      <Link
+        href="/favorites"
         className="bg-gray-800 hover:bg-gray-700 text-white p-2 rounded-full transition duration-300"
-        onClick={() => router.push("/favorites")}
       >
         <StarIcon width={24} height={24} />
-      </button>
-      <button
+      </Link>
+      <Link
+        href="/messages"
         className="bg-gray-800 hover:bg-gray-700 text-white p-2 rounded-full transition duration-300"
-        onClick={() => router.push("/messages")}
       >
         <ChatboxIcon width={24} height={24} />
-      </button>
-      <button
+      </Link>
+      <Link
+        href="/profile"
         className="bg-gray-800 hover:bg-gray-700 text-white p-2 rounded-full transition duration-300"
-        onClick={() => router.push("/profile")}
       >
         <PersonIcon width={24} height={24} />
-      </button>
+      </Link>
     </div>
   );
 };
