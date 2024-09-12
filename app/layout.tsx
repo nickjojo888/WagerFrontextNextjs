c7cea143fe4f227b16ae96c7910d91b8b133af26
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-09-12 17:58:50 +0100

    update the site so outside menu not scrollable

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-900 text-white overflow-x-hidden`}
+        className={`${inter.className} bg-gray-900 text-white overflow-hidden`}
       >
         <AuthProvider>
           <LoadingController>
