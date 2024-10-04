fa702ee064d9a7f7b8d874b264315c4af5001c29
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-10-04 23:31:44 +0100

    update loading spinner size based on space of parent

--- app/components/site_loading/LoadingScreen.tsx
+++ app/components/site_loading/LoadingScreen.tsx
@@ -2,19 +2,18 @@ import React from "react";
 
 const LoadingScreen: React.FC = () => {
   const videoHtml = `
-    <video autoplay loop muted class="w-32 h-32">
+    <video autoplay loop muted class="w-1/12 h-auto">
       <source src="/videos/WagerAnimation.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   `;
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center bg-black">
-      <div
-        dangerouslySetInnerHTML={{ __html: videoHtml }}
-        suppressHydrationWarning
-      />
-    </div>
+    <div
+      dangerouslySetInnerHTML={{ __html: videoHtml }}
+      suppressHydrationWarning
+      className="w-full h-full flex flex-col items-center justify-center bg-black"
+    ></div>
   );
 };
 
