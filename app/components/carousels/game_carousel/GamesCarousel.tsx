f1c26db4301c4334e11248968c10436d8b44d65f
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-10-05 12:33:39 +0100

    directly fetch images from the source without server dside optimisation to improve performance adn decrease server bloat

--- app/components/carousels/game_carousel/GamesCarousel.tsx
+++ app/components/carousels/game_carousel/GamesCarousel.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React, { useRef, useState, useEffect } from "react";
 import CarouselButton from "../CarouselButton";
-import Image from "next/image";
 import Link from "next/link";
 import { Game } from "./getGames";
+
+// Add this custom loader function
+const customLoader = ({ src }: { src: string }) => {
+  return src;
+};
 
 interface GamesCarouselProps {
   provider: string;
@@ -108,13 +112,12 @@ const GamesCarousel: React.FC<GamesCarouselProps> = ({
               href={`/games/${provider}/${game.id}`}
               className="w-1/3 xs:w-1/4 md:w-1/5 lg:w-1/7 xl:w-1/8 flex-shrink-0 snap-start transition-transform duration-300 ease-in-out hover:-translate-y-2"
             >
-              <Image
+              <img
                 src={game.imageUrl}
                 alt={game.name}
-                width={200}
-                height={200}
                 style={{ border: game.border }}
                 className="rounded-lg w-full h-auto"
+                loading="lazy"
               />
             </Link>
           ))
--- app/components/game_display/GameDisplay.tsx
+++ app/components/game_display/GameDisplay.tsx
@@ -21,13 +21,12 @@ export default function GameDisplay({ games, provider }: GameDisplayProps) {
             href={`/games/${provider}/${game.id}`}
             className="w-1/2 xs:w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-1/8 flex-shrink-0 transition-transform duration-300 ease-in-out hover:-translate-y-2"
           >
-            <Image
+            <img
               src={game.imageUrl}
               alt={game.name}
-              width={200}
-              height={200}
               style={{ border: game.border }}
               className="rounded-lg w-full h-auto"
+              loading="lazy"
             />
           </Link>
         ))}
