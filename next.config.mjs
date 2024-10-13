03106ce9a4b5f037fe895ee23fff6a1136c1662c
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-10-13 11:56:41 +0100

    update backend url env variable if on other device in local network

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+import { networkInterfaces } from "os";
+
+function getServerIP() {
+  const nets = networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name] ?? []) {
+      if (net.family === "IPv4" && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return "localhost";
+}
+
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -14,6 +28,14 @@ const nextConfig = {
     });
 
     return config;
+  },
+  env: {
+    // if the backend url env is local host, then replace with server ip so that testing on mobile using laptop backend
+    NEXT_PUBLIC_BACKEND_URL:
+      process.env.NEXT_PUBLIC_BACKEND_URL?.replace(
+        "localhost",
+        getServerIP()
+      ) || "",
   },
 };
 
